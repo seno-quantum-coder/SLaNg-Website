baafb0cf15a0fa4442b216e42a8a8af5b40325fc
SENODROOM <136210526+SENODROOM@users.noreply.github.com>
2026-05-26 16:00:29 +0500

    fixed the vite . cofig .js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [svelte()],
+  define: {
+    'process.argv': '["", ""]'
+  },
   resolve: {
     alias: {
       slangmath: resolve('src/lib/slangmath-demo.js')
